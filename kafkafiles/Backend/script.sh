d839818fb5c570f58c54dc39a139464c807b1745
sakshi2912 <sakshiv700@gmail.com>
2020-07-15 18:21:05 +0530

    update scripts

--- kafkafiles/Backend/script.sh
+++ kafkafiles/Backend/script.sh
@@ -1,6 +1,6 @@
 sudo systemtl start docker
 sudo docker rm $(sudo docker ps -aq)
-sudo dokcker rmi $(sudo docker images)
+sudo docker rmi $(sudo docker images)
 cd consumer
 sudo docker build -t consumer .
 sudo docker run -p 8050:8050 -p 9092:9092 consumer 
--- kafkafiles/Intermediate/script.sh
+++ kafkafiles/Intermediate/script.sh
@@ -1,9 +1,9 @@
 sudo systemctl start docker
 sudo docker rm $(sudo docker ps -aq)
-sudo dokcker rmi $(sudo docker images)
+sudo docker rmi $(sudo docker images)
 cd kafka-broker
 sudo docker compose up -d
 cd ..
 cd producer
 sudo docker build -t producer .
-sudo docekr run --net kaf-net producer
+sudo docker run --net kaf-net producer -d
