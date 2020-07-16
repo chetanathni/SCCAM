e69377896bfb9cb43f7832407dee6d21ab6642f8
sakshi2912 <sakshiv700@gmail.com>
2020-07-16 23:22:00 +0530

    Clean

--- kafkafiles/Intermediate/kafka-broker/README.md
+++
@@ -1,3 +0,0 @@
-1. Change KAFKA_ADVERTISED_HOST_NAME to external IP address of Intermediate VM.
-2. Run docker-compose file
-> sudo docker-compose up
--- kafkafiles/Intermediate/script.sh
+++
@@ -1,9 +0,0 @@
-sudo systemctl start docker
-sudo docker rm $(sudo docker ps -aq)
-sudo docker rmi $(sudo docker images)
-cd kafka-broker
-sudo docker compose up -d
-cd ..
-cd producer
-sudo docker build -t producer .
-sudo docker run --net kaf-net producer -d
