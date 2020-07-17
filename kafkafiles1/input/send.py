50ecdf15ef6794841fdcd79917ba47e4571f924b
sakshi2912 <sakshiv700@gmail.com>
2020-07-17 17:28:48 +0530

    add mongo

--- kafkafiles1/input/send.py
+++ kafkafiles1/input/send.py
@@ -1,14 +1,24 @@
 from kafka import KafkaConsumer
-
+from kafka import KafkaProducer
+from pymongo import MongoClient 
+import json
 bootstrap_servers=['kafka:9092']
+db_server='mongodb:27017'
+topicName ='sample'
 
-TopicName ='InputImage'
-consumer= KafkaConsumer (TopicName, group_id = 'group2', bootstrap_servers = bootstrap_servers, api_version = (0,10,0), auto_offset_reset = 'latest')
-consumer.subscribe(TopicName)
+consumer = KafkaConsumer (topicName, group_id = 'test-consumer-group',bootstrap_servers = bootstrap_servers,api_version=(0,10,0),auto_offset_reset = 'latest',value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+consumer.subscribe(topicName)
+
+conn = MongoClient(db_server, 27017)
+db = conn.database 
+collection = db.all_data 
+cursor = collection.find() 
+
 
 for message in consumer:
 	print(message.value)
+	collection.insert_one(message.value) 
 
+#for record in cursor: 
+#    print(record) 
 
-
-	
