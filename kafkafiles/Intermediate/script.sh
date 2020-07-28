315eece40a397eb39ddb36f4f62d934cf33641a8
sakshi2912 <sakshiv700@gmail.com>
2020-07-28 16:41:54 +0530

    modify script

--- kafkafiles/Intermediate/script.sh
+++ kafkafiles/Intermediate/script.sh
@@ -1,9 +1,17 @@
+curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip > a
+ip_add=$(cat a)
+rm a
+sudo sed -i '$ d' ~/.profile
+sudo echo "IP_ADD=$ip_add" >> ~/.profile
+.  ~/.profile
+sudo echo "KAFKA_ADVERTISED_HOST_NAME: $ip_add" >> .env
 sudo systemctl start docker
 sudo docker rm $(sudo docker ps -aq)
 sudo docker rmi $(sudo docker images)
 cd kafka-broker
-sudo docker compose up -d
+sudo docker-compose up -d
+sleep 10
 cd ..
 cd producer
-sudo docker build -t producer .
-sudo docker run --net kaf-net producer -d
+sudo docker build -d -t producer .
+sudo docker run --net kaf-net producer
