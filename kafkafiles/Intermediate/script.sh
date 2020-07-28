683601ce23fbf60e5fc8197824a0dbdb273648b0
sakshi2912 <51375116+sakshi2912@users.noreply.github.com>
2020-07-28 15:41:23 +0530

    Update script.sh

--- kafkafiles/Intermediate/script.sh
+++ kafkafiles/Intermediate/script.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip > a
+ip_add=$(cat a)
+rm a
+sudo sed -i '$ d' ~/.profile
+sudo echo "IP_ADD=$ip_add" >> ~/.profile
+.  ~/.profile
+
 sudo systemctl start docker
 sudo docker rm $(sudo docker ps -aq)
 sudo docker rmi $(sudo docker images)
