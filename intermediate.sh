2f34816d039ecef37a29bb0977f9e79fcdea3b10
sakshi2912 <sakshiv700@gmail.com>
2020-07-30 12:16:14 +0530

    Added env to auto-add ip

--- intermediate.sh
+++ intermediate.sh
@@ -1,3 +1,8 @@
+ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' > a
+IP_ADD=$(cat a)
+rm a
+VAR="KAFKA_ADVERTISED_HOST_NAME=$IP_ADD"
+sed -i "/KAFKA_ADVERTISED_HOST_NAME/c $VAR" ./.env
 sudo systemctl start docker
 sudo docker-compose down
 echo "Starting up broker"
@@ -7,11 +12,11 @@ sleep 60
 #echo "Sending Dummy data"
 #sudo docker build -t pro .
 #sleep 20
-#sudo docker run -d --net package_default pro
+#sudo docker run -d --net sccam_default pro
 #cd ..
 cd ./kafkafiles1/filter
 echo "Buliding Intermediate Container"
 sudo docker build -t filter .
 sleep 30
 echo "Intermediate Running , you can see DB object IDs"
-sudo docker run --net package_default filter
+sudo docker run --net sccam_default filter
